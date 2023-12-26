a420d05a2bf5f01cb682d33b122da28aa4f24fcb
Ritipong <ritipong.w@gmail.com>
2023-12-26 15:19:02 +0700

    change builttitle method

--- lib/Adapter_screen.dart
+++ lib/Adapter_screen.dart
@@ -2,14 +2,17 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_blue_plus/flutter_blue_plus.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:location/location.dart';
 import 'snackbar.dart';
 
 
 class AdaptorsOffScreen extends StatelessWidget {
-  const AdaptorsOffScreen({Key? key, this.adapterState}) : super(key: key);
+  const AdaptorsOffScreen({Key? key, this.adapterState, this.GPSadapterState}) : super(key: key);
 
   final BluetoothAdapterState? adapterState;
+
+  final ServiceStatus? GPSadapterState;
 
   Widget buildAdapterOffIcon(BuildContext context, {required IconData setIcon}) {
     return Icon(
@@ -19,8 +22,8 @@ class AdaptorsOffScreen extends StatelessWidget {
     );
   }
 
-  Widget buildTitle(BuildContext context, {required String adapterTitle}) {
-    String? state = adapterState?.toString().split(".").last;
+  Widget buildTitle(BuildContext context, {required String adapterTitle, required String state}) {
+    //String? state = adapterState?.toString().split(".").last;
     return Text(
       '$adapterTitle Adapter is ${state != null ? state : 'not available'}',
       style: Theme.of(context).primaryTextTheme.titleSmall?.copyWith(color: Colors.white),
@@ -33,12 +36,14 @@ class AdaptorsOffScreen extends StatelessWidget {
       child: ElevatedButton(
         child: const Text('TURN ON'),
         onPressed: () async {
+          GeolocatorPlatform _geolocatorPlatform = GeolocatorPlatform.instance;
+          Location location = new Location();
           try {
             if (Platform.isAndroid) {
-              await FlutterBluePlus.turnOn();
+              await location.requestService();
             }
           } catch (e) {
-            Snackbar.show(ABC.a, prettyException("Error Turning On:", e), success: false);
+            Snackbar.show(ABC.b, prettyException("Error Turning On:", e), success: false);
           }
         },
       ),
@@ -77,10 +82,13 @@ class AdaptorsOffScreen extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   buildAdapterOffIcon(context, setIcon: Icons.bluetooth_disabled),
+                  Padding(
+                    padding: EdgeInsets.all(10), // หรือใส่ค่าที่ต้องการ
+                  ),
                   Column(
                     children: [
                       if (Platform.isAndroid) buildBlueToothTurnOnButton(context),
-                      buildTitle(context, adapterTitle: 'BlueTooth'),
+                      buildTitle(context, adapterTitle: 'BlueTooth', state: adapterState.toString().split(".").last),
                     ],
                   ),
                 ],
@@ -92,10 +100,16 @@ class AdaptorsOffScreen extends StatelessWidget {
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   buildAdapterOffIcon(context,setIcon: Icons.location_disabled),
+                  Padding(
+                    padding: EdgeInsets.all(10), // หรือใส่ค่าที่ต้องการ
+                  ),
                   Column(
                     children: [
                       if (Platform.isAndroid) buildGPSTurnOnButton(context),
-                      buildTitle(context, adapterTitle: 'Location'),
+                      if(GPSadapterState == ServiceStatus.enabled)
+                        buildTitle(context, adapterTitle: 'Location', state: 'on'),
+                      if(GPSadapterState == ServiceStatus.disabled)
+                        buildTitle(context, adapterTitle: 'Location', state: 'off'),
                     ],
                   ),
                 ],
